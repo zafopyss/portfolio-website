2aa1c3b68d1a1f3981c892daff2d2a8ab046da86
zafopyss <107508095+zafopyss@users.noreply.github.com>
2025-12-08 15:51:32 +0100

    add more technologies and fix div height (#3)

--- src/components/design/TechCard/TechCard.tsx
+++ src/components/design/TechCard/TechCard.tsx
@@ -9,37 +9,37 @@ export default function TechCard({ name, icon }: TechCardProps) {
     return (
         <CursorSpotlight clipBorderRadius="rounded-xl">
             <div className="relative flex flex-col items-center justify-center
-                h-50 w-50 rounded-xl bg-neutral-900
+                h-30 w-30 rounded-xl bg-neutral-900
                 transition-all duration-200
                 hover:bg-neutral-800
                 overflow-hidden
                 group">
                 
-                <span className="pointer-events-none absolute left-0 top-[10%] h-[80%] w-px
+                <span className="pointer-events-none absolute left-0 top-[10%] h-[85%] w-px
                     bg-white/60 
                     opacity-0 
-                    group-hover:opacity-100 
+                    group-hover:opacity-20 
                     transition-opacity duration-300" 
                 />
-                <span className="pointer-events-none absolute right-0 top-[10%] h-[80%] w-px
+                <span className="pointer-events-none absolute right-0 top-[10%] h-[85%] w-px
                     bg-white/60 
                     opacity-0 
-                    group-hover:opacity-100 
+                    group-hover:opacity-20 
                     transition-opacity duration-300" 
                 />
+
                 {/* Top border */}
                 <span className="pointer-events-none absolute left-[10%] right-[10%] top-0 h-px
                     bg-white/60 
                     opacity-0 
-                    group-hover:opacity-80 
+                    group-hover:opacity-15 
                     transition-opacity duration-300" 
                 />
-
                 {/* Bottom border */}
                 <span className="pointer-events-none absolute left-[10%] right-[10%] bottom-0 h-px
                     bg-white/60 
                     opacity-0 
-                    group-hover:opacity-80 
+                    group-hover:opacity-15 
                     transition-opacity duration-300" 
                 />
 
--- src/data/techs.ts
+++ src/data/techs.ts
@@ -1,17 +1,30 @@
 
+import ansibleLogo from '../assets/ansible-logo.svg';
 import awsLogo from '../assets/aws-logo.svg';
+import djangoLogo from '../assets/django-logo.svg';
 import dockerLogo from '../assets/docker-logo.svg';
+import fastapiLogo from '../assets/FastAPI-logo.svg';
 import javascriptLogo from '../assets/js-logo.svg';
+import minioLogo from '../assets/minio-logo.svg';
+import postgresqlLogo from '../assets/PostgreSQL-logo.svg';
 import pythonLogo from '../assets/python-logo.svg';
-import reactLogo from '../assets/react.svg';
+import reactLogo from '../assets/react-logo.svg';
+import tailwindLogo from '../assets/tailwind-logo.svg';
+import terraformLogo from '../assets/terraform-logo.svg';
 import typescriptLogo from '../assets/Typescript-logo.svg';
-
 
 export const techs = [
     { name: "Python", icon: pythonLogo },
+    { name: "Django", icon: djangoLogo },
+    { name: "FastAPI", icon: fastapiLogo },
+    { name: "Tailwind", icon: tailwindLogo },
     { name: "JavaScript", icon: javascriptLogo },
-    { name: "React", icon: reactLogo },
     { name: "TypeScript", icon: typescriptLogo },
+    { name: "React", icon: reactLogo },
+    { name: "PostgreSQL", icon: postgresqlLogo },
+    { name: "MinIO", icon: minioLogo },
     { name: "Docker", icon: dockerLogo },
     { name: "AWS", icon: awsLogo },
+    { name: "Terraform", icon: terraformLogo },
+    { name: "Ansible", icon: ansibleLogo },
 ];
